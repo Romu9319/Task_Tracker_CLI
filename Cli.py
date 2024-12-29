7aeb8d4e53e29deaedaa8d53b24710d9442f805d
unknown <danielerm1510@gmail.com>
2024-12-29 21:30:59 +0100

    properties added to function ADD

--- Cli.py
+++ Cli.py
@@ -1,5 +1,6 @@
 import json_manager
 import click
+import datetime
 
 
 @click.group()
@@ -16,10 +17,12 @@ def list():
 
 @cli.command()
 @click.option('--description', required=True, help="Write task description")
+@click.option('--status', default="in-progress", help="Task creation date")  # done, todo, in-progress
+@click.option('--date', default=datetime.date.today(), help="Task creation date")
 ### investigar como puedo hacer para que se rellenen los datos del 
 ### resto de propiedades de la tarea ("status","createdAt", "updatedAt")
 @click.pass_context
-def add(ctx, description):
+def add(ctx, description, status, date):
     if not description:
         ctx.fail("Task description required")
     else:
@@ -28,10 +31,13 @@ def add(ctx, description):
         new_task = {
             "id": task_id,
             "description": description,
+            "status":"in-progress",
+            "createdAt": str(date),
+            "updatedAt": str(datetime.date.today())
+
         } 
         data.append(new_task)
         json_manager.add_task(data)
-
 
 if __name__ == '__main__':
     cli()
